73911db3a2fc380d2e0351afc8d0716bec004dc5
timdaniëls <timdanielscode@hotmail.com>
2023-09-12 10:54:27 +0000

    Added register.php file with register form fields

---
+++ register.php
@@ -0,0 +1,26 @@
+<form action="" method="POST">
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Voornaam:</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Achternaam:</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Studentennummer:</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email:</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Wachtwoord:</label>
+    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Wachtwoord herhalen:</label>
+    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+</form>
