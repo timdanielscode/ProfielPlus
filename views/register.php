ea2c54c806e57beea396fe04ee5a503300d34cf5
timdanielscode <timdanielscode@hotmail.com>
2023-09-14 14:39:22 +0000

    Update register view

--- views/register.php
+++ views/register.php
@@ -10,22 +10,22 @@
   <div class="form-group">
     <label for="lastName">Achternaam:</label>
     <input id="lastName" name="lastName" type="text" class="form-control">
-    <?php if(!empty($errors['firstName'])) { echo $errors['firstName']; } ?>
+    <?php if(!empty($errors['lastName'])) { echo $errors['lastName']; } ?>
   </div>
   <div class="form-group">
     <label for="email">Email:</label>
     <input id="email" name="email" type="email" class="form-control">
-    <?php if(!empty($errors['firstName'])) { echo $errors['firstName']; } ?>
+    <?php if(!empty($errors['email'])) { echo $errors['email']; } ?>
   </div>
   <div class="form-group">
     <label for="password">Wachtwoord:</label>
     <input id="password" name="password" type="password" class="form-control">
-    <?php if(!empty($errors['firstName'])) { echo $errors['firstName']; } ?>
+    <?php if(!empty($errors['password'])) { echo $errors['password']; } ?>
   </div>
   <div class="form-group">
     <label for="retypePassword">Wachtwoord herhalen:</label>
     <input id="retypePassword" name="retypePassword" type="password" class="form-control">
-    <?php if(!empty($errors['firstName'])) { echo $errors['firstName']; } ?>
+    <?php if(!empty($errors['retypePassword'])) { echo $errors['retypePassword']; } ?>
   </div>
   <input type="submit" name="submit" value="Submit"/>
 </form>
