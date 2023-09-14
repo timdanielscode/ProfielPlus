c07596db4174f32f594559e044eae4e939bc1a66
timdanielscode <timdanielscode@hotmail.com>
2023-09-14 11:09:30 +0000

    Added import.sql file and added users table

---
+++ database/import.sql
@@ -0,0 +1,13 @@
+/*
+    USERS TABLE
+*/
+
+CREATE TABLE users (
+    id int(11) AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    retypePassword VARCHAR(255) NOT NULL,
+    createdAt DATETIME NOT NULL,
+    updatedAt DATETIME NOT NULL
+);
