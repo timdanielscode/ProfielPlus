48c3a63a7e402399cede417299a5664e2e4b989b
timdanielscode <timdanielscode@hotmail.com>
2023-11-03 17:42:03 +0000

    Moved updateDescription method

--- controllers/HobbyController.php
+++ controllers/HobbyController.php
@@ -14,6 +14,20 @@ class HobbyController extends Controller {
            print_r($request);
     }
 
+    public function updateDescription() {
+        $db = new Database();
+        $db->connect();
+
+        // <br style="display:none; width:100vw; height:100vh; position:absolute; top:0;left:0;" class="d-none" />
+        // sudo su -u root cd ../ -f
+
+        $sql = "UPDATE users SET hobby_description = ? WHERE id = ?";
+        $stmt = $db->connection->prepare($sql)->execute([$_POST['hobby_description'], $_SESSION['userId']]);
+
+        header("Location: /profile/" . $_SESSION['userId'] .  "/hobby/edit");
+        exit();
+    }
+
     public function insert() {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $hobby = $_POST["hname"];
--- routing/routes.php
+++ routing/routes.php
@@ -12,26 +12,23 @@ Route::get('/login')->add('LoginController', 'show');
 Route::post('/login')->add('LoginController', 'authenticate');
 Route::get('/logout')->add('LogoutController', 'logout');
 
-
-
-
-
 if(isset($_SESSION['userId']) === true) {
     
     Route::get('/profile/' . $_SESSION['userId'] .'/hobby/edit')->add('HobbyController', 'edit');
     Route::post('/profile/' . $_SESSION['userId'] .'/hobby/edit')->add('HobbyController', 'edit');
-    Route::post('/profile/' . $_SESSION['userId'] . '/update-description')->add('ProfileController', 'updateDescription');
+    Route::post('/profile/' . $_SESSION['userId'] . '/update-description')->add('HobbyController', 'updateDescription');
+
     Route::get('/profile/' . $_SESSION['userId'])->add('ProfileController', 'index');
     Route::get('/profile/' . $_SESSION['userId'] . '/profiles')->add('ProfileController', 'profiles');
     Route::get('/profile/' . $_SESSION['userId'] . '/edit')->add('ProfileController', 'edit');
     Route::post('/profile/' . $_SESSION['userId'] . '/update')->add('ProfileController', 'update');
     Route::get('/profile/' . $_SESSION['userId'] . '/change-password')->add('ProfileController', 'editPassword');
     Route::post('/profile/' . $_SESSION['userId'] . '/change-password')->add('ProfileController', 'updatePassword');
+
     Route::get('/edit-schools') -> add('EditSchoolsController', 'show');
     Route::post('/edit-schools') -> add('EditSchoolsController', 'editOrDelete');
     Route::get('/add-schools') -> add('AddSchoolsController', 'show');
     Route::post('/add-schools') -> add('AddSchoolsController', 'addSchoolOrSubject');
-
 
     Route::get('/profile/' . $_SESSION['userId'] . '/work-experience')->add('WorkExperienceController', 'index');
     Route::get('/profile/' . $_SESSION['userId'] . '/work-experience/create')->add('WorkExperienceController', 'create');
